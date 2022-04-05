594d6a63722b82afda784d0954ca44bfa847abca
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2022-04-05 14:27:27 +0200

    Add example of error style bleeding

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Hello World</h1>
+  <div>
+    <nuxt-page/>
+
+    <div style="display: flex; gap: 8px;">
+      <nuxt-link to="/">Index</nuxt-link>
+      <nuxt-link to="/404">404</nuxt-link>
+    </div>
+  </div>
 </template>
