7e94e90263923e30cce91c61a1e65ea1dbcd44d3
Arkadiusz Sygulski <arkadiusz@sygulski.pl>
2022-04-05 14:59:07 +0200

    Add example for error throwing

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Hello World</h1>
+  <div>
+    <nuxt-page/>
+
+    <div style="display: flex; gap: 8px;">
+      <nuxt-link to="/">Index</nuxt-link>
+      <nuxt-link to="/throw-error">Throw Error</nuxt-link>
+    </div>
+  </div>
 </template>
---
+++ pages/throw-error.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Throw Error</h1>
+</template>
+
+<script lang="ts" setup>
+  import { throwError } from '#app';
+
+  const error = new Error('Error in setup via throwError');
+  throwError(error);
+
+  throw new Error('Error in setup via throw');
+</script>
